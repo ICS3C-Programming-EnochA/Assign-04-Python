7e860ceccaf07e5365de7d4d080889d39f6d3d9a
Enoch Amedjrovi <enoch.amedjrovi@stu.ocsb.ca>
2025-05-02 03:10:20 +0000

    positive_integer.py

--- positive_integer.py
+++ positive_integer.py
@@ -6,9 +6,11 @@
 
 def main():
 
+    #It will print out the initial greeting
     print ("Hello and welcome to Enoch math problem")
     user_num_as_string = input("Enter a positive integer = ")
-
+    
+    # we are using a try statement 
     try:
         num = int(user_num_as_string)
         if num > 0:
@@ -19,7 +21,7 @@ def main():
             print("Enter positive numbers only.")
     except:
         print(f"{user_num_as_string} Enter a valid input.")
-
-
+    finally:
+        print("Thank you for playing")
 if __name__ == "__main__":
     main()
